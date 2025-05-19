8b535a324e06b7cd5aa155300a8d9018d3255f7c
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2025-05-19 16:45:00 -0700

    testing

--- src/app/(public)/(auth)/auth/callback/route.ts
+++ src/app/(public)/(auth)/auth/callback/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
+  console.log(origin);
   const code = searchParams.get("code");
   const next = searchParams.get("next") ?? "/analytics";
   if (!code) {
