aca65d5a84242d45a71562de99d596db3309192b
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2025-05-19 15:52:20 -0700

    Add console for testing

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,9 +5,8 @@ export const apiClient = ofetch.create({
   baseURL: isBrowser ? "/api/v1" : process.env.API_URL,
   async onRequest({ options }) {
     if (isBrowser) return;
-    console.log("onRequest after isBrowser");
     const { headers } = await import("next/headers");
-    console.log("onRequest after import");
     options.headers = await headers();
+    console.log("onRequest after headers");
   },
 });
