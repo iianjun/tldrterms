bfd8cb9322ac15a0893ec0ec95819c2334a11eef
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2025-06-05 16:36:45 +0900

    Fix error

--- src/app/api/v1/health/route.ts
+++ src/app/api/v1/health/route.ts
@@ -1,6 +1,9 @@
 import { CustomResponse } from "@/lib/response";
+import { createClient } from "@/lib/supabase/server";
 
 export async function GET() {
+  const supabase = await createClient();
+  await supabase.auth.getUser();
   return CustomResponse.success({
     data: "OK",
   });
